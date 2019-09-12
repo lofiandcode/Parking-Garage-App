4b69d01c7f5fd01ec3c43ee59101b9d835063e8a
github username <josephtrettevik@gmail.com>
2019-09-11 18:35:37 -0700

    Trying to get edit page to work

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -23,34 +23,36 @@ class CarsController < ApplicationController
         params[:car][:user_ids].each do |user_id| 
             UserCar.create(user_id: user_id, car: @car)
         end
-        byebug
+        # byebug
         redirect_to car_path(@car)
     end
     
     def edit
+        # byebug
         @car = Car.find(params[:id])
-        @garage = Garage.find(car_params[:garage_id])
-        @car = Car.update(
-            make: car_params[:make], 
-            model: car_params[:model],
-            license_plate: car_params[:license_plate],
-            garage: @garage
-        )
-        car_params[:user_id].each do |user_id| 
-            UserCar.find_or_create_by(user_id: user_id, car: @car)
-        end
     end
 
     def update
-        @car = Car.find(params[:id])
-        @car.update(car_params)
+        # @garage = Garage.find(car_params[:garage_id])
+        if @car = Car.update( make: car_params[:make], model: car_params[:model], license_plate: car_params[:license_plate], garage: @garage)
+            puts "Updated"
+        else
+            puts "did not update"
+            render :edit
+        end
+        byebug
+        params[:car][:user_ids].each do |user_id| 
+            puts "Hi 45"
+            UserCar.find_or_create_by(user_id: user_id, car: @car)
+        end
+    
         redirect_to car_path(@car)
     end
 
     private
 
     def car_params
-        params.require(:car).permit(:make, :model, :license_plate, :garage_id)
+        params.require(:car).permit(:make, :model, :license_plate, :garage_id, :id)
     end
 
     # def user_params
